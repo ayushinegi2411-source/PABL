d3cd2ff541e68b45f655183eb62c44739540eaf9
ayushinegi2411 <ayushinegi2411@gmail.com>
2026-02-16 13:25:05 +0530

    updated

--- homework assignment/h11.py
+++ homework assignment/h11.py
@@ -3,11 +3,11 @@
 def is_subset(a, b):
     freq = {}
 
-    # Count elements of a
+   
     for num in a:
         freq[num] = freq.get(num, 0) + 1
 
-    # Check elements of b
+   
     for num in b:
         if num not in freq or freq[num] == 0:
             return False
